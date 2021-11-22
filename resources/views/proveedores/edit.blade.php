945f6a411070a6de5efc22a1582bc7447696b6ad
CamiloQuintero1 <camilobaena@hotmail.es>
2021-11-22 01:47:06 -0500

    Vistas terminadas

--- resources/views/proveedores/edit.blade.php
+++ resources/views/proveedores/edit.blade.php
@@ -15,7 +15,7 @@
 
 <div class="col">
 <label for="" class="form-label">Nit/Cedula</label>   
-<input disabled minlength="10" maxlength="10" onkeypress="return event.charCode>= 48&& event.charCode <=57"  id="nit" name="nit" type="text" class="form-control" value="{{$proveedores->nit}}" tabindex="1"> 
+<input disabled minlength="10" maxlength="10" onkeypress="return event.charCode>= 48&& event.charCode <=57"  id="nit" name="nit" type="text" class="form-control" value="{{$proveedores->nit}}"> 
 <!-- @if($errors->has('nit'))
 <span class="error text-danger" for="input-name">{{$errors->first('nit')}}</span>
 @endif -->
@@ -24,7 +24,7 @@
 
 <div class="col">
 <label for="" class="form-label">Nombre Contacto</label>   
-<input  minlength="3" maxlength="30" id="nombrecontacto" name="nombrecontacto" type="text" class="form-control" value="{{$proveedores->nombrecontacto}}" tabindex="2"> 
+<input  minlength="3" maxlength="30" id="nombrecontacto" name="nombrecontacto" type="text" class="form-control" value="{{$proveedores->nombrecontacto}}"> 
 </div>
 </div> 
 
@@ -34,7 +34,7 @@
 <div class="row mb-3">
 <div class="col">
 <label for="" class="form-label">Correo C </label>   
-<input id="correocontacto" name="correocontacto" type="text" class="form-control" value="{{ old('correocontacto',$proveedores->correocontacto) }}" tabindex="3"> 
+<input id="correocontacto" name="correocontacto" type="text" class="form-control" value="{{ old('correocontacto',$proveedores->correocontacto) }}"> 
 @if($errors->has('correocontacto'))
 <span class="error text-danger" for="input-name">{{$errors->first('correocontacto')}}</span>
 @endif
@@ -44,35 +44,40 @@
 
 <div class="col">
 <label for="" class="form-label">Numero Contacto</label>   
-<input minlength="3" maxlength="15"  onkeypress="return event.charCode>= 48&& event.charCode <=57" id="numerocontacto"  name="numerocontacto" type="text" class="form-control" value="{{$proveedores->numerocontacto}}"tabindex="4"> 
+<input minlength="3" maxlength="15"  onkeypress="return event.charCode>= 48&& event.charCode <=57" id="numerocontacto"  name="numerocontacto" type="text" class="form-control" value="{{$proveedores->numerocontacto}}"> 
 </div>
 </div>
+
+
 
 
 
 <div class="row mb-3">
 <div class="col">
 <label for="" class="form-label">Empresa</label>   
-<input  disabled id="empresa" name="empresa" type="text" class="form-control" value="{{$proveedores->empresa}}" tabindex="5"> 
+<input  disabled id="empresa" name="empresa" type="text" class="form-control" value="{{$proveedores->empresa}}"> 
 </div>
 
 
 <div class="col">
 <label for="" class="form-label">Direccion Empresa</label>   
-<input id="direccionempresa" name="direccionempresa" type="text" class="form-control" value="{{$proveedores->direccionempresa}}" tabindex="6"> 
+<input id="direccionempresa" name="direccionempresa" type="text" class="form-control" value="{{$proveedores->direccionempresa}}"> 
 </div>
 
 
-</div>   
-</div>  
+
+
+</div>    
         </div>
       </div>  
-    </div>     
+    </div>   
+    <a href="/proveedores" class="btn btn-secondary"tabindex="5"><i class="fas fa-backward"></i></a>
+    <button style="float: right;"  type="submit" class="btn btn-success"  tabindex="4"><i class="fas fa-check"></i></button> 
+    </div>   
 
 
     
-    <a href="/proveedores" class="btn btn-secondary" tabindex="7" ><i class="fas fa-backward"></i></a>
-    <button style="float: right;"  type="submit" class="btn btn-success"  tabindex="8"><i class="fas fa-check"></i></button>
+    
 
 
 
