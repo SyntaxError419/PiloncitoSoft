195c67284c049b2e0bbdb9f7abb881723f67020a
CamiloQuintero1 <camilobaena@hotmail.es>
2021-11-24 04:32:32 -0500

    Obligatorios

--- resources/views/proveedores/create.blade.php
+++ resources/views/proveedores/create.blade.php
@@ -9,12 +9,12 @@
        <h2 class="pt-3">Crear proveedor</h2>
        <div class="card mt-4"> 
        <div class="card-header"> 
-      
+       <p class="text-danger">* Campo obligatorio.</p>
         <div class="card-body"> 
         <div class="row mb-3"> 
 
                 <div class="col"> 
-                  <label for="" class="form-label">Nit/Cedula</label>
+                  <label for="" class="form-label">Nit/Cedula</label><label class="text-danger"> *</label>
                   <input minlength="10" maxlength="10"  placeholder="Ingrese NIT o Cedula" value="{{ old('nit')}}" onkeypress="return event.charCode>= 48&& event.charCode <=57" id="nit" name="nit" type="text" class="form-control" tabindex="1"> 
                   @if($errors->has('nit'))
                   <span class="error text-danger" for="input-name">{{$errors->first('nit')}}</span>
@@ -23,7 +23,7 @@
   
             
                 <div class="col">
-                <label for="" class="form-label">Nombre Contacto</label>   
+                <label for="" class="form-label">Nombre Contacto</label><label class="text-danger"> *</label>   
                 <input  placeholder="Ingrese el Nombre y Apellidos" value="{{ old('nombrecontacto')}}"   minlength="3" maxlength="15" id="nombrecontacto" name="nombrecontacto" type="text" class="form-control" tabindex="2"> 
                 @if($errors->has('nombrecontacto'))
                 <span class="error text-danger" for="input-name">{{$errors->first('nombrecontacto')}}</span>
@@ -34,7 +34,7 @@
 
               <div class="row mb-3">
               <div class="col">
-              <label for="" class="form-label">Correo C</label>   
+              <label for="" class="form-label">Correo Contacto</label><label class="text-danger"> *</label>   
               <input  placeholder="Ingrese el Correo Electronico" value="{{ old('correocontacto')}}"   id="correocontacto" name="correocontacto" type="text" class="form-control" tabindex="3"> 
                @if($errors->has('correocontacto'))
               <span class="error text-danger" for="input-name">{{$errors->first('correocontacto')}}</span>
@@ -44,7 +44,7 @@
                 
                 
               <div class="col">
-              <label for="" class="form-label">Numero Contacto</label>   
+              <label for="" class="form-label">Numero Contacto</label><label class="text-danger"> *</label>   
               <input minlength="7" maxlength="10" placeholder="Ingrese el Numero De telefono" value="{{ old('numerocontacto')}}"    onkeypress="return event.charCode>= 48&& event.charCode <=57" id="numerocontacto" name="numerocontacto" type="text" class="form-control" tabindex="4"> 
               @if($errors->has('numerocontacto'))
              <span class="error text-danger" for="input-name">{{$errors->first('numerocontacto')}}</span>
@@ -55,7 +55,7 @@
 
             <div class="row mb-3"> 
             <div class="col">
-            <label for="" class="form-label">Empresa</label>   
+            <label for="" class="form-label">Empresa</label><label class="text-danger"> *</label>   
             <input  placeholder="Ingrese la Empresa" value="{{ old('empresa')}}"  id="empresa" name="empresa" type="text" class="form-control" tabindex="5"> 
             @if($errors->has('empresa'))
             <span class="error text-danger" for="input-name">{{$errors->first('empresa')}}</span>
@@ -64,7 +64,7 @@
             
 
             <div class="col"> 
-            <label for="" class="form-label">Direccion Empresa</label>   
+            <label for="" class="form-label">Direccion Empresa</label><label class="text-danger"> *</label>   
             <input  placeholder="Ingrese La Direccion de la Empresa" value="{{ old('direccionempresa')}}"  id="direccionempresa" name="direccionempresa" type="text" class="form-control" tabindex="6"> 
             @if($errors->has('direccionempresa'))
            <span class="error text-danger" for="input-name">{{$errors->first('direccionempresa')}}</span>
